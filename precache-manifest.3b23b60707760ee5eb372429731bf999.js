b3833b88451aca48b908e55072a697b0468136d4
Anton <maskafolk@yandex.ru>
2020-09-16 13:10:21 +0300

    Updates

---
+++ precache-manifest.3b23b60707760ee5eb372429731bf999.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d95a2d1c73589a48bc87fa7f236e08c8",
+    "url": "/jack-react-with-my-ts/index.html"
+  },
+  {
+    "revision": "c80d355345a1d7703956",
+    "url": "/jack-react-with-my-ts/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "b3afcc11d15132083062",
+    "url": "/jack-react-with-my-ts/static/js/2.6afba834.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/jack-react-with-my-ts/static/js/2.6afba834.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c80d355345a1d7703956",
+    "url": "/jack-react-with-my-ts/static/js/main.a56135e4.chunk.js"
+  },
+  {
+    "revision": "7db8ebf15ff03a06da62",
+    "url": "/jack-react-with-my-ts/static/js/runtime-main.cf73fe56.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/jack-react-with-my-ts/static/media/logo.5d5d9eef.svg"
+  }
+]);
